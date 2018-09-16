6d1af2a1574fc9dde25b14411b2860a445b561c4
Deathmetall <40876487+tomasmetal23@users.noreply.github.com>
2018-09-16 13:30:44 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,147 +1,85 @@
-<!DOCTYPE html>
-<html>
-<meta charset="utf-8" />
-<head>
-<title>Banano</title>
-<link rel="stylesheet" type="text/css" href="index.css">
-</head>
-<body onload="onLoad();">
-  <div id="banano"></div>
-  <ol>
-    <li><img class="small_image" src="https://i.imgur.com/tQwwFx2.jpg" /></li>
-  </ol>
-  <table>
-    <tr>
-      <th>Id</th>
-      <th>Name</th>
-    </tr>
-    <tr>
-      <td>1</td>
-      <td>deathmetall</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>tomasmetal23</td>
-    </tr>
-  </table>
-  <script>
-      const addChildText = (parent,childText) => {
-        const node = document.createTextNode(childText);
-        parent.appendChild(node);
-        return node;
-      }
+const buttonIds = [ 
+  {'id':'vl','chance':0.01,'payout':98},
+  {'id':'l','chance':0.23,'payout':5},
+  {'id':'a','chance':0.48,'payout':1},
+  {'id':'h','chance':0.73,'payout':0.25},
+  {'id':'vh','chance':0.98,'payout':0.01}
+];
+
+const onLoad = () => {
+  html('win_chance', '-');
+  html('roll_under', '-');
+  html('payout', '-');
+  setOdds();
+};
+
+const playClick = (elt) => {
+  const json = [];
   
-      const addChildElement = (parent,childType,childText) => {
-        const child = document.createElement(childType);
-        parent.appendChild(child);
-        if(childText !== undefined) {
-          child.appendChild(document.createTextNode(childText));
-        }
-        return child;
-      }
+  const numberOfBets = value('number_of_bets');
+  const serverSeed = value('server_seed');
+  const clientSeed = value('client_seed');
+  const nonce = value('nonce');
   
-	  const loadJson = () => {
-        const xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-          if (this.readyState == 4 && this.status == 200) {
-            loadJsonCallback(this.response);
-          }
-        }
-        xhttp.responseType = 'json';
-        xhttp.open('GET', 'index.json', true);
-        xhttp.send();
-      }
-      const loadJsonCallback = (json) => {
-        const banano = document.getElementById('banano');
-        
-        addChildText(banano, 'Deathmetall Bananos');
-        const table = addChildElement(banano,'table');
-        
-        const tableHeaderRow = addChildElement(table,'tr');
-        if(json.length == 0) {
-          return;
-        }
-        
-        for (const [key, value] of Object.entries(json[0])) {
-          addChildElement(tableHeaderRow,'th',key);
-        }
-        
-        json.forEach((jsonElt,jsonEltIx) => {
-          const tableDataRow = addChildElement(table,'tr');
-          for (const [key, value] of Object.entries(jsonElt)) {
-            addChildElement(tableDataRow,'td',value);
-          }
-        });
-      }
-      const onLoad = () => {
-        loadJson();
-      }
-    </script>
-</body>
-</html>
+  const target = parseFloat(html('roll_under'));
+  const payout = parseFloat(html('payout'));
+  
+  let totalProfit = 0;
+  
+  for(let ix = 0; ix < numberOfBets; ix++) {
+    const number = (parseInt(ix) + parseInt(nonce));
+    
+    const jsonElt = {};
 
-.small_image {
-  height: 100px;
-  width: 100px;
+    const message = clientSeed + ':' + number;
+    
+    const hash = CryptoJS.HmacSHA512(message, serverSeed).toString().substring(1,6);
+
+    const result = ((parseInt(hash,16) % 10000) / 100).toFixed(2);
+    
+    const wonFlag = result < target;
+
+    const won = wonFlag ? 'won' : 'lost';
+    
+    const profit = wonFlag ? payout : -1;
+    
+    totalProfit += profit;
+    
+    jsonElt['Bet Number'] = number;
+    jsonElt['Hash'] = hash;
+    jsonElt['Result'] = result;
+    jsonElt['Target'] = target;
+    jsonElt['Win/Lose'] = won;
+    jsonElt['Profit'] = profit;
+    jsonElt['Total Profit'] = totalProfit.toFixed(2);
+    
+    json.push(jsonElt);
+  }
+  
+  const id = elt.getAttribute('id');
+  html('bets','');
+  createTable('bets',json);
+};
+
+const chanceSelectionClick = (elt) => {
+  buttonIds.forEach((button) => {
+    class_attr(button.id, 'button');
+  });
+  const id = elt.getAttribute('id');
+  if (class_attr(id) == 'button selected_button') {
+    class_attr(id, 'button');
+  } else {
+    class_attr(id, 'button selected_button');
+  }
+  setOdds();
 }
 
-table, tr, td, th {
-  border: solid;
-  border-width: 0.5px;
-  font-family: monospace;
-}
-
-
-.text_align_center{
-  text-align: center;
-}
-
-.w100pct {
-  width: 100%;
-}
-
-.w99pct {
-  width: 99%;
-}
-
-.w96pct {
-  width: 96%;
-}
-
-.w600px {
-  width: 600px;
-}
-
-.selected_button {
-  box-shadow: 0px -2px #999;
-}
-
-.unselected_button {
-  box-shadow: 0 0px #999;
-}
-
-.button {
-  padding: 3px 3px;
-  margin: 3px 3px;
-  font-size: 12px;
-  text-align: center;
-  cursor: pointer;
-  outline: none;
-  color: #000;
-  background-color: #EEE;
-  border: solid;
-  border-width: 0.5px;
-  border-radius: 5px;
-  border-color: #AAA;
-}
-
-.button:hover {
-  background-color: #DDD;
-  box-shadow: 0px 2px #999;
-}
-
-.button:active {
-  background-color: #EEE;
-  box-shadow: 0 1px #666;
-  transform: translateY(2px);
+const setOdds = () => {
+  buttonIds.forEach((button) => {
+    if (class_attr(button.id) == 'button selected_button') {
+      html('win_chance', button.chance);
+      html('roll_under', button.chance * 100);
+      html('payout', button.payout);
+    }
+  });
 }
